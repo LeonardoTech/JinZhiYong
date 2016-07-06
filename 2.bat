55f5a3b76f948d9cf2a82367ee27c92384b6e569
OkcZhiyong <invoker1994@sina.com>
2016-07-06 12:05:02 +0800

    test

--- 2.bat
+++ 2.bat
@@ -1,10 +1,3 @@
-@echo off
-git add .
-git commit -m "test"
-
-    for /f "delims=" %%a in (D:\JinZhiyong\Tag.txt) do (
-    git tag %%a
-    ) 
-git push
-
-    pause
+@echo off 
+for /f "tokens=1,4 delims= " %%i in (D:\JinZhiyong\.git\FETCH_HEAD) do echo %%i
+pause
---
+++ test.bat
@@ -0,0 +1,4 @@
+@echo off
+set /p a=<D:\JinZhiyong\.git\FETCH_HEAD
+echo %a%
+pause>nul
